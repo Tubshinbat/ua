01f8005026da30090f2a5f63ac4ced910f6c5b60
Tuvshinbat <tuvshinbat596@gmail.com>
2022-02-15 18:20:21 +0800

    blog image add

--- components/home-three-news.js
+++ components/home-three-news.js
@@ -6,14 +6,14 @@ import "swiper/css/navigation";
 import "swiper/css/scrollbar";
 import "swiper/css/autoplay";
 
-import { EffectFade, Navigation, Autoplay } from "swiper";
+import { EffectFade, Navigation, Autoplay, Scrollbar } from "swiper";
 
 export default () => {
   return (
     <div className="home__three_news container">
       <Swiper
         className={`news__three_slider`}
-        modules={[Navigation, Autoplay]}
+        modules={[EffectFade, Navigation, Scrollbar, Autoplay]}
         slidesPerView={3}
         spaceBetween={40}
         autoplay={{
@@ -22,6 +22,10 @@ export default () => {
         navigation={{
           prevEl: ".news__three_slider_prev",
           nextEl: ".news__three_slider_next",
+        }}
+        scrollbar={{
+          el: ".news__three_scrollbar",
+          draggable: false,
         }}
       >
         <SwiperSlide className={`three_slide`}>
@@ -63,7 +67,7 @@ export default () => {
         <SwiperSlide className={`three_slide`}>
           <div className="news__box_t">
             <div className="news__box_t_image">
-              <img src="/images/blog-4.jpg" />
+              <img src="/images/blog-5.jpg" />
             </div>
             <div className="news__box_desciption">
               <div className="news__box_date">
@@ -74,16 +78,55 @@ export default () => {
                   <i class="fa fa-bolt"></i> 126 үзсэн
                 </div>
               </div>
-              <h5>Пэрэнлэйн Оюун-Эрдэнэ докторын дипломоо гардан авлаа</h5>
+              <h5>Сар шинийн мэндчилгээ</h5>
             </div>
           </div>
         </SwiperSlide>
         <SwiperSlide className={`three_slide`}>
-          <div>4</div>
+          <div className="news__box_t">
+            <div className="news__box_t_image">
+              <img src="/images/blog-6.jpg" />
+            </div>
+            <div className="news__box_desciption">
+              <div className="news__box_date">
+                <div className={`news__date_item`}>
+                  <i class="fa-regular fa-clock"></i>2 сарын өмнө
+                </div>
+                <div className={`news__date_item`}>
+                  <i class="fa fa-bolt"></i> 126 үзсэн
+                </div>
+              </div>
+              <h5>Нээлттэй ажлын байрны зар</h5>
+            </div>
+          </div>
         </SwiperSlide>
         <SwiperSlide className={`three_slide`}>
-          <div>6</div>
+          <div className="news__box_t">
+            <div className="news__box_t_image">
+              <img src="/images/blog-7.jpg" />
+            </div>
+            <div className="news__box_desciption">
+              <div className="news__box_date">
+                <div className={`news__date_item`}>
+                  <i class="fa-regular fa-clock"></i>2 сарын өмнө
+                </div>
+                <div className={`news__date_item`}>
+                  <i class="fa fa-bolt"></i> 126 үзсэн
+                </div>
+              </div>
+              <h5>Цахим сургалт боллоо</h5>
+            </div>
+          </div>
         </SwiperSlide>
+        <div className="news__three_bottom">
+          <div className="news__three_scrollbar_box">
+            <div className="news__three_scrollbar swiper-scrollbar"></div>
+          </div>
+          <div className="news__three_nav">
+            <div className="news__three_prev swiper-button-prev"></div>
+            <div className="news__three_next swiper-button-next"></div>
+          </div>
+        </div>
       </Swiper>
     </div>
   );
