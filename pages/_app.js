1cd410fbeb75f6511a2e707162f622a9b8d8278c
Tubshinbat <tuvshinbat596@gmail.com>
2022-06-09 15:14:02 +0800

    mtag

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,8 @@ const WOW = !isServer ? require("wow.js") : null;
 import { useCookies } from "react-cookie";
 import Router from "next/router";
 import Nprogress from "nprogress";
+import { useRouter } from "next/router";
+import * as gtag from "lib/gtag";
 
 import TimeAgo from "javascript-time-ago";
 import mn from "javascript-time-ago/locale/mn.json";
@@ -58,6 +60,19 @@ function MyApp({ Component, pageProps }) {
     return res.json();
   };
 
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    router.events.on("hashChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+      router.events.off("hashChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <SWRConfig
@@ -98,14 +113,23 @@ function MyApp({ Component, pageProps }) {
 
           <Script src="/js/all.min.js" crossorigin />
           <Script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-B2E3N5ZCZQ"
-          ></Script>
-          <Script>
-            window.dataLayer = window.dataLayer || []; function gtag()
-            {dataLayer.push(arguments)}
-            gtag('js', new Date()); gtag('config', 'G-B2E3N5ZCZQ');
-          </Script>
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+          />
+          <Script
+            id="gtag-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
         </Head>
         <CustomCursor />
         <Component {...pageProps} />
